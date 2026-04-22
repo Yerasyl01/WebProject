e47d7362b12bf4d601c8021a9fed3438095a69e2
Yerasyl01 <erabay336@outlook.com>
2026-04-23 02:41:03 +0500

    update

--- backend/business_uchet/settings.py
+++ backend/business_uchet/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'business_uchet',
         'USER': 'postgres',
-        'PASSWORD': 'REMOVED',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
